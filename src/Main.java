714ea95dc1187d3bb9753b62a8a5790120e99aca
carlos.guaita <carlos.guaita@udla.edu.ec>
2025-03-28 17:37:44 -0500

    Se agrega clase materia

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import Models.Estudiante;
+import Models.Materia;
+
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,15 +9,47 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Estudiante est1 = new Estudiante();
-        est1.setNombre("Juan");
-        est1.setEdad(20);
-        est1.setNotas(8.5, 9.0, 7.5);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese los datos del estudiante:");
+        System.out.print("Nombre: ");
+        String nombre = sc.next();
+        System.out.print("Edad: ");
+        int edad = sc.nextInt();
+        System.out.print("Nota1: ");
+        double nota1 = sc.nextDouble();
+        System.out.print("Nota2: ");
+        double nota2 = sc.nextDouble();
+        System.out.print("Nota3: ");
+        double nota3 = sc.nextDouble();
 
-        System.out.println("Nombre: " + est1.getNombre());
-        System.out.println("Edad: " + est1.getEdad());
-        double notas[] = est1.getNotas();
-        System.out.println("Nota1: " + notas[0] + " Nota2: " + notas[1] + " Nota3: " + notas[2]);
+        Estudiante est1 = new Estudiante();
+        est1.setNombre(nombre);
+        est1.setEdad(edad);
+        est1.setNotas(nota1, nota2, nota3);
+
+        System.out.println("Ingrese los datos de la materia:");
+        System.out.print("Nombre de la materia: ");
+        String nombreMateria = sc.next();
+        System.out.print("Creditos: ");
+        int creditos = sc.nextInt();
+        System.out.print("Codigo: ");
+        String codigo = sc.next();
+
+        Materia mt1 = new Materia();
+        mt1.setNombre(nombreMateria);
+        mt1.setCreditos(creditos);
+        mt1.setCodigo(codigo);
+        mt1.setEstudiante(est1);
+
+        System.out.println("Nombre del estudiante: " + mt1.getEstudiante().getNombre());
+        System.out.println("Edad del estudiante: " + mt1.getEstudiante().getEdad());
+        double[] notas = mt1.getEstudiante().getNotas();
+        System.out.println("Notas del estudiante: ");
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Nota " + (i + 1) + ": " + notas[i]);
+        }
+
+
 
 
 
